8f4f0890a974eea2c9b7a9186f17eb89bae1a052
EkinoksAhmet <80361129+EkinoksAhmet@users.noreply.github.com>
2022-04-19 23:32:27 +0300

    Navbar düzeltme denemesi

--- scripts/navbarControl.js
+++ scripts/navbarControl.js
@@ -10,3 +10,18 @@ navToggle.addEventListener('click', () => {
         primaryNav.setAttribute("data-visible", "false");
     }
 });
+
+function navControl() {
+    const primaryNav = document.querySelector('.primary-nav');
+    const navToggle = document.querySelector('.mobile-nav-toggle');
+
+    navToggle.addEventListener('click', () => {
+        const visibility = primaryNav.getAttribute('data-visible');
+
+        if (visibility === "false") {
+            primaryNav.setAttribute("data-visible", "true");
+        } else if (visibility === "true") {
+            primaryNav.setAttribute("data-visible", "false");
+        }
+});
+}
